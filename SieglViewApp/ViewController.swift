41484ac832cc6d3d24180576a9099642698bed73
CernohorskyVaclav <vaclav.cernohorsky@gmail.com>
2019-11-05 13:42:04 +0100

    Initial Commit

---
+++ SieglViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SieglViewApp
+//
+//  Created by Václav Černohorský on 05/11/2019.
+//  Copyright © 2019 Václav Černohorský. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
